5501ec7639a7ce95498a671aecf88adf8fa35b63
pchristophermnwilliams <qhqzmkc0459@outlook.com>
2025-08-04 20:40:32 +0000

    code

--- scripts/dpo_config.py
+++ scripts/dpo_config.py
@@ -4,36 +4,36 @@ from copy import deepcopy
 
 DPO_CONFIG = {
     "0_1_b": {
-        "lr": 1.35e-5,
+        "lr": 1.4e-5,
         "distributed": "ddp",
         "gpu_count": 1,
         "batch_size": 16,
     },
     "1_2_b": {
-        "lr": 8.7e-6,
+        "lr": 8.9e-6,
         "distributed": "ddp",
         "gpu_count": 1,
         "batch_size": 12,
     },
     "2_4_b": {
-        "lr": 6.5e-6,
+        "lr": 6.9e-6,
         "distributed": "ddp",
         "gpu_count": 2,
         "batch_size": 12,
         "use_lora": True
     },
     "4_5_b": {
-        "lr": 6.25e-6,
+        "lr": 6.6e-6,
         "distributed": "ddp",
         "gpu_count": 4,
         "batch_size": 12,
         "use_lora": True
     },
     "5_9_b": {
-        "lr": 7.5e-6,
+        "lr": 7.8e-6,
         "distributed": "ddp",
         "gpu_count": 4,
-        "batch_size": 8,
+        "batch_size": 12,
         "use_lora": True
     },
     "9_12_b": {
@@ -53,7 +53,7 @@ DPO_CONFIG = {
         "gradient_checkpointing": False
     },
     "14_15_b": {
-        "lr": 8.5e-6,
+        "lr": 8.9e-6,
         "distributed": "ds",
         "gpu_count": 8,
         "use_lora": True,
@@ -61,7 +61,7 @@ DPO_CONFIG = {
         "gradient_checkpointing": False
     },
     "15_40_b": {
-        "lr": 8e-6,
+        "lr": 8.3e-6,
         "distributed": "ds",
         "gpu_count": 8,
         "use_lora": True,
@@ -69,7 +69,7 @@ DPO_CONFIG = {
         "gradient_checkpointing": False
     },
     "40_80_b": {
-        "lr": 8e-6,
+        "lr": 8.8e-6,
         "distributed": "ds",
         "gpu_count": 8,
         "use_lora": True,
@@ -190,7 +190,7 @@ def get_training_json(train_info: dict) -> dict:
         "epoch_num": 3,
         "batch_size": config["batch_size"],
         "learning_rate": config["lr"],
-        "min_lr_rate": 0.25,
+        "min_lr_rate": 0.28,
         "use_liger": get_use_liger(model_architecture),
         "optimizer": "paged_adamw_8bit",
         "use_lora": config.get("use_lora", False),
--- scripts/grpo_config.py
+++ scripts/grpo_config.py
@@ -4,21 +4,21 @@ from copy import deepcopy
 
 GRPO_CONFIG = {
     "0_1_b": {
-        "lr": 0.00015,
+        "lr": 0.00018,
         "distributed": "ddp",
         "gpu_count": 1,
         "batch_size": 40,
         "vllm_gpu_memory_utilization": 0.4
     },
     "1_2_b": {
-        "lr": 7.5e-5,
+        "lr": 7.8e-5,
         "distributed": "ddp",
         "gpu_count": 1,
         "batch_size": 40,
         "vllm_gpu_memory_utilization": 0.4
     },
     "2_4_b": {
-        "lr": 8e-5,
+        "lr": 8.5e-5,
         "distributed": "ddp",
         "gpu_count": 2,
         "batch_size": 42,
@@ -26,7 +26,7 @@ GRPO_CONFIG = {
         "use_lora": True
     },
     "4_5_b": {
-        "lr": 8e-5,
+        "lr": 8.6e-5,
         "distributed": "ddp",
         "gpu_count": 2,
         "batch_size": 42,
@@ -34,7 +34,7 @@ GRPO_CONFIG = {
         "vllm_gpu_memory_utilization": 0.4
     },
     "5_6_b": {
-        "lr": 6e-5,
+        "lr": 6.5e-5,
         "distributed": "ddp",
         "gpu_count": 2,
         "batch_size": 42,
@@ -42,7 +42,7 @@ GRPO_CONFIG = {
         "vllm_gpu_memory_utilization": 0.4
     },
     "6_9_b": {
-        "lr": 6e-5,
+        "lr": 6.9e-5,
         "distributed": "ddp",
         "gpu_count": 4,
         "batch_size": 24,
@@ -50,7 +50,7 @@ GRPO_CONFIG = {
         "vllm_gpu_memory_utilization": 0.5
     },
     "9_12_b": {
-        "lr": 6.5e-5,   
+        "lr": 6.9e-5,   
         "distributed": "ddp",
         "gpu_count": 4,
         "use_lora": True,
@@ -62,7 +62,7 @@ GRPO_CONFIG = {
         "distributed": "ddp",
         "gpu_count": 4,
         "use_lora": True,
-        "batch_size": 2,
+        "batch_size": 8,
         "vllm_gpu_memory_utilization": 0.8,
     },
     "15_20_b": {
@@ -75,7 +75,7 @@ GRPO_CONFIG = {
         "use_vllm": False,
     },
     "20_40_b": {
-        "lr": 8e-5,
+        "lr": 8.5e-5,
         "distributed": "ddp",
         "gpu_count": 8,
         "use_lora": True,
@@ -85,7 +85,7 @@ GRPO_CONFIG = {
         "use_4bit": True
     },
     "40_80_b": {
-        "lr": 8e-5,
+        "lr": 8.5e-5,
         "distributed": "ddp",
         "gpu_count": 8,
         "use_lora": True,
@@ -230,7 +230,7 @@ def get_training_json(train_info: dict) -> dict:
         "epoch_num": 2,
         "batch_size": config["batch_size"],
         "learning_rate": config["lr"],
-        "min_lr_rate": 0.25,
+        "min_lr_rate": 0.28,
         "use_liger": get_use_liger(model_architecture),
         "optimizer": "paged_adamw_8bit",
         "use_lora": config.get("use_lora", False),
